668f514d85525526bf8561af0556112ba5fa1a41
brianwlazlo <73142386+brianwlazlo@users.noreply.github.com>
2021-02-03 16:43:54 -0500

    Add files via upload

---
+++ List.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './list';
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<List/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+  .create(<List />).toJSON();
+  expect(tree).toMatchSnapshot();  
+});
